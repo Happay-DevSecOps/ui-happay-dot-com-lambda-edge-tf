1ff8aa8d29a73c67e49a11692651649926f15341
vivekyad4v <vivekyad4v@gmail.com>
2021-05-24 17:05:02 +0530

    changed few items that CDN doesn't support for origin request lambda edge

--- lambda/main.tf
+++ lambda/main.tf
@@ -10,25 +10,20 @@ resource "aws_lambda_function" "function" {
   role             = var.iam_role_arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.7"
-  timeout          = "60"
+  timeout          = "10"
   publish = true
-
-  environment {
-    variables = {
-      CLOUDWATCH_LOG_GROUP_ARN = var.cloudwatch_log_group_arn,
-     }
-   }
 
   tags 			=   merge(var.tags, tomap({
                             "description" = "${local.name_prefix} Lambda function",
-                            "Name" = "${local.name_prefix}-function"}))
+                            "Name" = "${local.name_prefix}-function",
+                            "LogGroup" = "${var.cloudwatch_log_group_arn}"}))
 }
 
 resource "aws_ssm_parameter" "lambda_function_arn" {
   name        = "/hpy/${local.environ}/${local.project}/lambda_function_arn"
   description = "${local.name_prefix} lambda_function_arn"
   type        = "String"
-  value       = aws_lambda_function.function.arn
+  value       = aws_lambda_function.function.qualified_arn
 
   tags 			=   merge(var.tags, tomap({
                             "description" = "${local.name_prefix} param store",
